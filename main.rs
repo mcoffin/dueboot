85f2900d85fe22b1229efe30b8ab91fa9cb803ce
Matt Coffin <mcoffin13@gmail.com>
2015-02-27 15:12:01 -0800

    Change LED to struct for demo purposes

--- main.rs
+++ main.rs
@@ -34,8 +34,30 @@ fn panic_fmt(args: core::fmt::Arguments,
     }
 }
 
+pub struct LED {
+    pin: u32,
+}
+
+impl LED {
+    pub fn new(pin: u32) -> LED {
+        let l = LED {
+            pin: pin,
+        };
+        l.set_pin_mode();
+        l
+    }
+
+    fn set_pin_mode(&self) {
+        pinMode(self.pin, OUTPUT);
+    }
+
+    pub fn set(&self, val: u8) {
+        digitalWrite(self.pin, val);
+    }
+}
+
 static PWM:u32 = 2;
-static LED:u32 = 13;
+static LED_PIN:u32 = 13;
 
 static PWM_LOW:u32 = 0;
 static PWM_HIGH:u32 = 16;
@@ -44,23 +66,23 @@ static PWM_HIGH:u32 = 16;
 pub fn main() {
     init();
     delay(1);
-    pinMode(LED, OUTPUT);
-    digitalWrite(LED, LOW);
+    let led = LED::new(LED_PIN);
+    led.set(LOW);
     analogWrite(PWM, PWM_LOW);
 
     loop {
         analogWrite(PWM, PWM_HIGH);
-        digitalWrite(LED, HIGH);
+        led.set(HIGH);
         delay(100);
         analogWrite(PWM, PWM_LOW);
-        digitalWrite(LED, LOW);
+        led.set(LOW);
         delay(100);
 
         analogWrite(PWM, PWM_HIGH);
-        digitalWrite(LED, HIGH);
+        led.set(HIGH);
         delay(1000);
         analogWrite(PWM, PWM_LOW);
-        digitalWrite(LED, LOW);
+        led.set(LOW);
         delay(1000);
-  }
+    }
 }
