eeaeea0c1818c62dfea5903fa90a2b0343a8fb5e
Matt Coffin <mcoffin13@gmail.com>
2015-02-27 13:50:24 -0800

    Forgot crate_type attribute in main

--- main.rs
+++ main.rs
@@ -1,3 +1,5 @@
+#![crate_type = "lib"]
+
 #![feature(asm)]
 #![feature(lang_items)]
 #![feature(no_std)]
@@ -7,21 +9,29 @@
 
 extern crate core;
 
-/*
 use arduino::{init, delay, pinMode, digitalWrite, analogWrite, LOW, HIGH, OUTPUT};
 mod arduino;
-*/
 
 #[lang = "stack_exhausted"]
 extern fn stack_exhausted() {}
+
 #[lang = "eh_personality"]
 extern fn eh_personality() {}
+
 #[lang = "panic_fmt"]
 #[allow(unused_variables)]
+/*
 fn panic_fmt(args: core::fmt::Arguments,
             file: &str,
             line: usize) ->  ! {
     loop {}
+}
+*/
+fn panic_fmt(args: core::fmt::Arguments,
+            file: &'static str,
+            line: usize) {
+    loop {
+    }
 }
 
 static PWM:u32 = 2;
@@ -32,7 +42,6 @@ static PWM_HIGH:u32 = 16;
 
 #[no_mangle]
 pub fn main() {
-    /*
     init();
     delay(1);
     pinMode(LED, OUTPUT);
@@ -54,5 +63,4 @@ pub fn main() {
         digitalWrite(LED, LOW);
         delay(1000);
   }
-  */
 }
